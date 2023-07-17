8f87f759ff9833a6efe3826cf560d55d94899037
HyunsooCha <729steven@gmail.com>
2023-07-17 20:50:51 +0900

    update 20:50:51 07-17-2023

--- scripts/command_20230713.sh
+++ scripts/command_20230713.sh
@@ -8,11 +8,12 @@ mkdir -p $SAVE_DIR
 SEED=100
 CFG_TXT=7.5
 CFG_IMG=1.3
-device=2
+device=1
 PROMPT="add a red baseball cap, skin color should not be red, high quality, with one's hat down, the color of the hat must be red, no decoration or logo on the hat, no need to fill the area, plausible, the hat must fit a person's head"
-total_frames=950
+start_frame=952
+total_frames=1842
 cd ..
-for ((frame_num=1;frame_num<=total_frames;frame_num++))
+for ((frame_num=start_frame;frame_num<=total_frames;frame_num++))
 do
     CUDA_VISIBLE_DEVICES=$device python edit_cli.py --input $INPUT_DIR/$frame_num.png --output $SAVE_DIR/$frame_num.png --edit "$PROMPT" --cfg-text $CFG_TXT --cfg-image $CFG_IMG --seed $SEED --resolution 256
 done
