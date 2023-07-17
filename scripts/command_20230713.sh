8d43bce1c43a903a54bafb7c9393707217ceab51
HyunsooCha <729steven@gmail.com>
2023-07-17 12:50:52 +0900

    update 12:50:52 07-17-2023

---
+++ scripts/command_20230713.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+set -e
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+INPUT_DIR=/home/hyunsoocha/GitHub/IMavatar/data/datasets/diffusion/diffusion/hyunsoo_1/image
+# SAVE_DIR=$SCRIPT_DIR/../results/mortar_board
+SAVE_DIR=/home/hyunsoocha/GitHub/IMavatar/data/datasets/diffusion/diffusion/hyunsoo_1/pix2pix
+mkdir -p $SAVE_DIR
+SEED=100
+CFG_TXT=7.5
+CFG_IMG=1.3
+device=2
+PROMPT="add a red baseball cap, skin color should not be red, high quality, with one's hat down, the color of the hat must be red, no decoration or logo on the hat, no need to fill the area, plausible, the hat must fit a person's head"
+total_frames=950
+cd ..
+for ((frame_num=1;frame_num<=total_frames;frame_num++))
+do
+    CUDA_VISIBLE_DEVICES=$device python edit_cli.py --input $INPUT_DIR/$frame_num.png --output $SAVE_DIR/$frame_num.png --edit "$PROMPT" --cfg-text $CFG_TXT --cfg-image $CFG_IMG --seed $SEED --resolution 256
+done
