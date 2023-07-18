98a80102fd2aba221ff9558be3734b525008e1f6
HyunsooCha <729steven@gmail.com>
2023-07-18 21:41:22 +0000

    update 21:41:22 07-18-2023

---
+++ install_conda.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+set -e
+
+source $HOME/anaconda3/etc/profile.d/conda.sh
+conda activate
+
+conda env create -f environment.yaml
+conda activate ip2p
---
+++ scripts/command_20230718_glasses_2.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+source $HOME/anaconda3/etc/profile.d/conda.sh
+conda activate
+conda activate ip2p
+set -e
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+INPUT_DIR=/home/hyunsoocha/GitHub/IMavatar/data/datasets/diffusion/diffusion/hyunsoo_1/image
+# SAVE_DIR=$SCRIPT_DIR/../results/mortar_board
+SAVE_DIR=/home/hyunsoocha/GitHub/IMavatar/data/datasets/diffusion/diffusion/hyunsoo_1/pix2pix_sunglasses
+mkdir -p $SAVE_DIR
+SEED=100
+CFG_TXT=7.5
+CFG_IMG=2.0
+device=5
+PROMPT="put on a pair of sunglasses"
+start_frame=901
+total_frames=1842
+cd ..
+for ((frame_num=start_frame;frame_num<=total_frames;frame_num++))
+do
+    CUDA_VISIBLE_DEVICES=$device python edit_cli.py --input $INPUT_DIR/$frame_num.png --output $SAVE_DIR/$frame_num.png --edit "$PROMPT" --cfg-text $CFG_TXT --cfg-image $CFG_IMG --seed $SEED --resolution 256
+done
