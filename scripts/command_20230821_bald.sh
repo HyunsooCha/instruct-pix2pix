ed0d442bd91b36b7344468f9819e77d05a082509
HyunsooCha <729steven@gmail.com>
2023-08-21 22:17:33 +0900

    update 22:17:33 08-21-2023

---
+++ scripts/command_20230821_bald.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+source $HOME/anaconda3/etc/profile.d/conda.sh
+conda activate
+conda activate ip2p
+set -e
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+BASE_DIR=$HOME/GitHub/IMavatar/data/datasets/paste/paste/hyunsoo_1
+INPUT_DIR=$BASE_DIR/image
+SAVE_DIR=$BASE_DIR/image_bald
+mkdir -p $SAVE_DIR
+SEED=100
+CFG_TXT=7.5
+CFG_IMG=2.0
+device=4
+PROMPT="make him a bald"
+start_frame=1
+total_frames=460
+cd ..
+for ((frame_num=start_frame;frame_num<=total_frames;frame_num++))
+do
+    CUDA_VISIBLE_DEVICES=$device python edit_cli.py --input $INPUT_DIR/$frame_num.png --output $SAVE_DIR/$frame_num.png --edit "$PROMPT" --cfg-text $CFG_TXT --cfg-image $CFG_IMG --seed $SEED --resolution 256
+done
